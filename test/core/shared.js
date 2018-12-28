2827225613d3d20824ea1a645d9a98107414da97
Stefan Rimaila <stefan@rimaila.fi>
2018-12-28 18:12:14 +0200

    test(core/shared): Add initial tests

--- test/core/shared.js
+++ test/core/shared.js
@@ -1,7 +1,19 @@
 const test = require('ava');
+const K = require('kefir');
 
 const P = require('../../src/core/shared');
 
+// #region [ Kefir ]
+test('property', t => {
+  t.true(P.property() instanceof K.Property);
+});
+
+test('pool', t => {
+  t.true(P.pool() instanceof K.Pool);
+})
+// #endregion
+
+// #region [ Strings ]
 test('tokensToCamel', t => {
   t.is('fooBar', P.tokensToCamel(['foo', 'bar']));
 });
@@ -9,3 +21,8 @@ test('tokensToCamel', t => {
 test('tokensToSnake', t => {
   t.is('foo_bar', P.tokensToSnake(['foo', 'bar']));
 });
+
+test('snakeToTokens', t => {
+  t.deepEqual(['foo', 'bar'], P.snakeToTokens('foo_bar'));
+});
+// #endregion
